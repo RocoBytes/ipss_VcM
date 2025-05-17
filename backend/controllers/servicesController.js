acbe89ab0c90ab8bf616863480deff594d456223
RocoBytes <rodrigo.contreras.rubio@estudiante.ipss.cl>
2025-05-17 17:41:22 -0400

    Feat: Add service creation logic with MongoDB integration using mongoose

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -1,10 +1,27 @@
 import {services} from "../data/beautyServices.js";
+import Services from "../models/Services.js";
 
+const createService = async (req, res) => {
+    if (Object.values(req.body).includes("")){
+        return res.status(400).json({message: "Todos los campos son obligatorios"});
+    }
+
+    try {
+        const service = new Services(req.body)
+        const result = await service.save();
+        res.status(201).json(result);
+
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
 
 const getServices = (req, res) => {
     res.json(services);
 };
 
 export {
+    createService,
     getServices
 }
--- backend/routes/servicesRoutes.js
+++ backend/routes/servicesRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getServices } from "../controllers/servicesController.js";
+import { createService, getServices } from "../controllers/servicesController.js";
 import { get } from "mongoose";
 
 const router = express.Router();
 
+router.post("/", createService);
 router.get("/", getServices);
+
 
 export default router;
