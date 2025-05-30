9f82d91e51fd2cb8f879396e9207314df55525d8
RocoBytes <rodrigo.contreras.rubio@estudiante.ipss.cl>
2025-05-29 23:57:35 -0400

    Feat: Add AppointmentsLayout with navigation and session UI for appointments section

--- frontend/src/views/appointments/AppointmentsLayout.vue
+++ frontend/src/views/appointments/AppointmentsLayout.vue
@@ -1,5 +1,5 @@
 <script setu>
-import { RouterView } from 'vue-router';
+import { RouterLink, RouterView } from 'vue-router';
 
 
 </script>
@@ -9,8 +9,34 @@ import { RouterView } from 'vue-router';
 
 <template>
 
-    <div>
-        <h1>Appointments Layout</h1>
+    <div class="flex justify-between">
+        <h1 class="text-2xl lg:text-6xl font-black text-white">Yohanna Rodriguez Studio</h1>
+
+        <div class="flex flex-col space-y-5">
+            <div class="flex gap-2 items-center"> 
+                <p class="text-white text-right">Hola: Usuario</p>
+
+                <button
+                    type="button"
+                    class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg"
+                >
+                    Cerrar sesión
+                </button>
+
+                <nav class="flex gap-2 items-center justify-end">
+                    <button
+                        class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg"
+                    >Mis Citas</button>
+
+                    <RouterLink
+                        :to="{name: 'new-appointment'}"
+                        class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700"
+                    >
+                        Nueva Cita
+                    </RouterLink>
+                </nav>
+            </div>
+        </div>
     </div>
 
 
