ff49fae7e39a129cf305b7f2e66c6a5ec34abbef
RocoBytes <rodrigo.contreras.rubio@estudiante.ipss.cl>
2025-06-01 09:10:12 -0400

    Feat(frontend): Add appointment summary and selected services display in AppointmentView

--- frontend/src/views/appointments/AppointmentView.vue
+++ frontend/src/views/appointments/AppointmentView.vue
@@ -1,8 +1,31 @@
 <script setup>
+    import { formatCurrency } from '@/helpers';
+import SelectedService from '../../components/SelectedService.vue';
+    import { useAppointmentsStore } from '../../stores/appointments';
+
+    const appointments = useAppointmentsStore()
 </script>
 
 <template>
     <div>
-        <h1>Detalles de la cita</h1>
+        <h2 class="text-4xl font-extrabold text-white mt-10">Detalles Cita y Resumen</h2>
+        <p class="text-white text-lg">A continuación verifica la información y confirma tu cita</p>
+    
+        <h3 class="text-3xl font-extrabold text-white mt-5">Servicios</h3>
+
+
+        <p v-if="appointments.noServicesSelected" class="text-white text-2xl text-center">No hay servicios seleccionados</p>
+
+        <div v-else class="grid gap-5 mt-5">
+            <SelectedService
+                v-for="service in appointments.services"
+                :key="service._id"
+                :service="service"
+            />
+
+            <p class="text-right text-white text-2xl">Total a pagar:
+                <span class="font-black"> {{ formatCurrency(appointments.totalAmount) }}</span>
+            </p>
+        </div>
     </div>
 </template>
--- frontend/src/views/appointments/NewAppointmentLayout.vue
+++ frontend/src/views/appointments/NewAppointmentLayout.vue
@@ -6,14 +6,12 @@
 
 
 <template> 
-
         <nav class="my-5 flex gap-3">
             <RouterLink
                 :to="{name: 'new-appointment'}"
                 class="flex-1 text-center p-3 uppercase font-extrabold hover:bg-blue-600 hover:text-white"
                 :class="route.name === 'new-appointment' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'"
-
-                >
+            >
                 Servicios
             </RouterLink>
 
@@ -22,14 +20,13 @@
                 class="flex-1 text-center p-3 uppercase font-extrabold hover:bg-blue-600 hover:text-white"
                 :class="route.name === 'appointment-details' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'"
 
-                >
+            >
                 Cita y Resumen
             </RouterLink>
-
         </nav>
-
-        <RouterView />
-
+        <div>
+            <RouterView />    
+        </div>
 </template>
 
 
