86d8a1efb68fa8228c7732912a113463c0d4974e
RocoBytes <rodrigo.contreras.rubio@estudiante.ipss.cl>
2025-06-01 11:55:00 -0400

    Feat(frontend): Add createAppointment function to appointments store

--- frontend/src/views/appointments/AppointmentView.vue
+++ frontend/src/views/appointments/AppointmentView.vue
@@ -11,6 +11,11 @@
         date: 'DD/MM/YYYY',
         month: 'MMM'
     })
+
+    const disableDate = (date) => {
+        const today = new Date()
+        return date < today || date.getMonth() > today.getMonth() + 1 /*|| [0,5].includes(date.getDay() ) */
+    }
 </script>
 
 <template>
@@ -37,17 +42,37 @@
 
         <div class="space-y-8" v-if="!appointments.noServicesSelected">
             <h3 class="text-3xl font-extrabold text-white">Fecha y Hora</h3>
+
             <div class="lg:flex gap-5 items-start">
                 <div class="w-full lg:w-96 bg-white flex justify-center rounded-lg"></div>
                 <VueTailwindDatepicker
+                    :disable-date="disableDate"
                     i18n="es-cl"
                     as-single
                     no-input
                     :formatter="formatter"
                     v-model="appointments.date"
                 />
-
             </div>
 
+            <div class="flex-1 grid grid-cols-1 xl:grid-cols-2 gap-5 mt-10 lg:mt-0">
+                <button
+                v-for="hour in appointments.hours"
+                class="block text-blue-500 rounded-lg text-xl font-black p-3"
+                :class="appointments.time === hour ? 'bg-blue-500 text-white' : 'bg-white'"
+                @click="appointments.time = hour"
+                
+            >
+            
+                {{ hour }}
+                </button>
+            </div>
+
+            <div v-if="appointments.isValidReservation" class="flex justify-end">
+                <button
+                    class="w-full md:w-auto bg-blue-500 p-3 rounded-lg uppercase font-black text-white"
+                    @click="appointments.createAppointment"
+                > Confirmar Reservación</button>
+            </div>
         </div>
 </template>
