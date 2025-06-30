e5e0a87629968712de5a99524e50b19904031f37
RocoBytes <rodrigo.contreras.rubio@estudiante.ipss.cl>
2025-06-30 11:35:20 -0400

    feat(frontend): agregar componente Appointment y limpieza de dependencias

---
+++ frontend/src/components/Appointment.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <p>Desde Citas</p>
+    </div>
+</template>
--- frontend/src/views/appointments/MyAppointmentsView.vue
+++ frontend/src/views/appointments/MyAppointmentsView.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import Appointment from '../../components/Appointment.vue'
     import { useUserStore } from '../../stores/user'
     const user = useUserStore()
 </script>
@@ -7,7 +8,20 @@
     <h2 class="text-4xl font-extrabold text-white mt-10">Mis citas</h2>
     <p class="text- text-lg  text-white mt-5">A continuación administrar tus próximas citas</p>
 
+    <p v-if="user.loading" class="text-white text-2xl text-center mt-5">Cargando...</p>
 
+    <div v-else>
+        <p v-if="user.noAppointments" class="text-white text-2xl text-center mt-5">No tienes próximas citas</p>
+
+        <div v-else class="grid grid-cols-1 gap-5 mt-10">
+            <Appointment
+                v-for="appointment in user.userAppointments"
+                :key="appointment._id"
+                :appointment="appointment"
+            />
+
+        </div>
+    </div>
 </template>
 
 
