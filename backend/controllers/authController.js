175075323b8506ce64b3fd6c2fdf60d785fd3028
RocoBytes <rodrigo.contreras.rubio@estudiante.ipss.cl>
2025-07-08 01:16:19 -0400

    Feat(Backend) admin route, end of proyect

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js'
-import { sendEmailVerification } from '../emails/authEmailService.js'
-import { generateJWT } from '../utils/index.js'
+import { sendEmailVerification, sendEmailPasswordReset } from '../emails/authEmailService.js'
+import { generateJWT, uniqueId } from '../utils/index.js'
 
 const register = async (req, res) => {
     
@@ -93,8 +93,80 @@ const login = async (req, res) => {
 
 }
 
+const forgotPassword = async(req,res) => {
+    const { email } = req.body
+
+    // Comprobar si existe el usuario
+    const user = await User.findOne({email})
+    if(!user){
+        const error = new Error('El usuario no existe')
+        return res.status(404).json({msg: error.message})
+    }
+
+    try {
+        user.token = uniqueId()
+        const result = await user.save()
+
+        await sendEmailPasswordReset({
+            name: result.name,
+            email: result.email,
+            token: result.token
+        })
+
+        res.json({
+            msg: 'Hemos enviado un email con las instrucciones'
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const verifyPasswordResetToken = async(req, res) => {
+    const { token } = req.params
+    const isValidToken = await User.findOne({token})
+    if(!isValidToken){
+        const error = new Error('Hubo un error, Token no válido')
+        return res.status(400).json({msg: error.message})
+    }
+
+    res.json({msg: 'Token valido'})
+}
+
+const updatePassword = async(req, res) => {
+    const { token } = req.params
+    const user = await User.findOne({token})
+    if(!user){
+        const error = new Error('Hubo un error, Token no válido')
+        return res.status(400).json({msg: error.message})
+    }
+
+    const { password } = req.body
+
+    try {
+        user.token = ''
+        user.password = password
+        await user.save()
+        res.json({
+            msg: 'Password modificado correctamente'
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const user = async(req, res) => {
     const { user } = req
+    res.json(
+        user
+    )
+}
+
+const admin = async(req, res) => {
+    const { user } = req
+    if(!user.admin){
+        const error = new Error('Acción no válida')
+        return res.status(403).json({msg: error.message})
+    }
     res.json(
         user
     )
@@ -105,5 +177,9 @@ export{
     register,
     verifyAccount,
     login,
-    user
+    forgotPassword,
+    verifyPasswordResetToken,
+    updatePassword,
+    user,
+    admin
 }
--- backend/emails/authEmailService.js
+++ backend/emails/authEmailService.js
@@ -24,3 +24,29 @@ export async function sendEmailVerification({name, email, token}){
 
     console.log('Mensaje enviado', info.messageId)
 }
+
+
+export async function sendEmailPasswordReset({name, email, token}){
+    const transporter = createTransport(
+        process.env.EMAIL_HOST,
+        process.env.EMAIL_PORT,
+        process.env.EMAIL_USER,
+        process.env.EMAIL_PASS
+    )
+
+    // Enviar el email
+    const info = await transporter.sendMail({
+        from: 'MEVN <contacto@yohanarodriguez.cl>',
+        to: email,
+        subject: 'Yohana Rodriguez Studio - Reestablece tu password',
+        text: 'Yohana Rodriguez Studio - Reestablece tu password',
+        html: `<p>Hola: ${name}, has solicitado reestablecer tu password</p>
+        <p>Sigue el siguiente encale para generar un nuevo password:</p>
+        <a href="${process.env.FRONTEND_URL}/auth/olvide-password/${token}">Reestablecer Password</a>
+        <p>Si tu no creaste esto, puedes ignorar este mensaje</p>
+
+        `
+    })
+
+    console.log('Mensaje enviado', info.messageId)
+}
